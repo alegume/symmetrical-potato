4a03febacd394fb686cc34ae6795a9316c357d8c
alegume <alexandreabreu@comp.ufla.br>
2019-03-19 15:42:51 -0300

    TemplateView, skeleton e stitics adicionados

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-	path('', views.post_list, name='post_list'),
+	path('post_list/', views.post_list, name='post_list'),
+	path('', TemplateView.as_view(template_name='blog/index.html')),
+	path('sobre/', TemplateView.as_view(template_name='blog/sobre.html')),
 ]
