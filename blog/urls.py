5d9305ac6b920fe49e98f614e687a3fe2a2b2365
alegume <alexandreabreu@comp.ufla.br>
2019-03-19 16:00:24 -0300

    url modificada

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-	path('post_list/', views.post_list, name='post_list'),
-	path('', TemplateView.as_view(template_name='blog/index.html')),
+	path('', views.post_list, name='post_list'),
+	path('home/', TemplateView.as_view(template_name='blog/index.html')),
 	path('sobre/', TemplateView.as_view(template_name='blog/sobre.html')),
 ]
