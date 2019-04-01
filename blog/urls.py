068494ebcf162f701fc18fd8eaefc573cafd9b0c
alegume <alexandreabreu@comp.ufla.br>
2019-04-01 20:06:02 -0300

    templates adicionados

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.post_list, name='post_list'),
+	path('post/<int:pk>', views.post_detail, name='post_detail'),
 	path('home/', TemplateView.as_view(template_name='blog/index.html')),
 	path('sobre/', TemplateView.as_view(template_name='blog/sobre.html')),
 ]
