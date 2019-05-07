97128d1b3948132fb584352deb6a84b575ad3db3
alegume <alexandreabreu@comp.ufla.br>
2019-05-07 17:59:41 -0300

    upload images/arquivos

--- meusite/urls.py
+++ meusite/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 # from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib.auth import views
 
@@ -12,3 +14,7 @@ urlpatterns = [
 	path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
 	path('accounts/', include('django.contrib.auth.urls'))
 ]
+
+# Url de arquivos staticos em modo de debug
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
